36e691b8c1ba712435d3795849cbd0fda68259e4
alejandrogomez3 <alejandro.gomez3@alumnos.uneatlantico.es>
2026-01-09 15:45:07 +0100

    Añadir los imports y mantener una estructura básica y clara

--- entregas/GómezAlejandro/src/GestorTareas.java
+++ entregas/GómezAlejandro/src/GestorTareas.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-class GestorTareas {
+public class GestorTareas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] tareas = new String[10];
